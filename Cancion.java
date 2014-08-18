93f0c51ae59670bc9e278b8e013e2d420fac9bf8
xXMAMBOXx <mar09f@gmail.com>
2014-08-18 01:46:14 -0600

    Set Get de class canción

--- Cancion.java
+++ Cancion.java
@@ -24,6 +24,27 @@ public class Cancion {
         
     }
     
+    public String getArtista(){
+    return this.Artista;
+    }
+    public String getTitulo(){
+    return this.Nombre;
+    }
+    public String getAlbum(){
+    return this.Album;
+    }
+    public int getduracion(){
+    return duracion;
+    }
+    public String getgenero(){
+    return this.genero;
+    }
+    public void setNombre(String m){
+    this.Nombre=m;
+    }
+    public void setArtista(String m){
+    this.Artista=m;
+    }
     
     
     
